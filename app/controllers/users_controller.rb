dc04c38876b06642239fb28e0a964609b87c9af0
rafik mrabet <mrabetr@hotmail.com>
2020-12-10 16:28:40 +0000

    removed commented lines

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
-  # before_action :require_admin! # your authorization method
 
   def index
-    # @users = User.order(:id)
     @users = policy_scope(User).order(:id)
   end
 
