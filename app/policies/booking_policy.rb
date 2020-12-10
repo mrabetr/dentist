937659b921276c0d8d7452afc1a709a8e888f1db
rafik mrabet <mrabetr@hotmail.com>
2020-12-10 15:00:04 +0000

    added booking pundit policy and refactored controller

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,46 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+      if user.doctor || user.admin
+        scope.all
+      else
+        scope.where(patient: user.profile)
+      end
+    end
+  end
+
+  def show?
+    doctor_admin_or_patient_owner?
+  end
+
+  def create?
+    doctor?
+  end
+
+  def update?
+    doctor_owner?
+  end
+
+  def destroy?
+    doctor_owner?
+  end
+
+  private
+
+  def doctor?
+    user.doctor
+  end
+
+  def doctor_owner?
+    user == record.doctor.user
+  end
+
+  # def doctor_or_admin?
+  #   user.doctor || user.admin
+  # end
+
+  def doctor_admin_or_patient_owner?
+    user.doctor || user.admin || user == record.patient.user
+  end
+end
