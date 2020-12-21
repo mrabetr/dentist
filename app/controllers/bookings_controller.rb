3879698f8a1492407d909da1eccff4591a8b952c
rafik mrabet <mrabetr@hotmail.com>
2020-12-21 14:33:24 +0000

    removed commented out code

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -11,7 +11,6 @@ class BookingsController < ApplicationController
   end
 
   def new
-    # @booking = Booking.new(date: params[:date], time: params[:time])
     @booking = Booking.new(start_time: params[:date], time: params[:time])
     @booking.booking_services.build
     authorize @booking
@@ -20,22 +19,12 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.doctor = current_user.profile
-    # raise
-    # @booking.start_time = DateTime.parse([@booking.date, @booking.time].join(' '))
     set_booking_times
-    # @booking.start_time = DateTime.parse("#{@booking.start_time.to_date}T#{@booking.time}")
-    # @booking.end_time = @booking.start_time + @booking.length.minutes
     authorize @booking
 
     return render_new unless @booking.save
 
     redirect_to bookings_path
-    # if @booking.save
-    #   redirect_to bookings_path
-    # else
-    #   flash[:alert] = @booking.errors.full_messages
-    #   render :new
-    # end
   end
 
   def edit
@@ -45,25 +34,10 @@ class BookingsController < ApplicationController
   def update
     return render_edit unless @booking.update(booking_params)
 
-    # @booking.start_time = DateTime.parse([@booking.date, @booking.time].join(' '))
     set_booking_times
-    # @booking.start_time = DateTime.parse("#{@booking.start_time.to_date}T#{@booking.time}")
-    # @booking.end_time = @booking.start_time + @booking.length.minutes
     return render_edit unless @booking.save
 
     redirect_to booking_path(@booking)
-
-    # if @booking.update(booking_params)
-    #   @booking.start_time = DateTime.parse([@booking.date, @booking.time].join(' '))
-    #   @booking.end_time = @booking.start_time + @booking.length.minutes
-    #   if @booking.save
-    #     redirect_to booking_path(@booking)
-    #   else
-    #     render :edit
-    #   end
-    # else
-    #   render :edit
-    # end
   end
 
   def destroy
