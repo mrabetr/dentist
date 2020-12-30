7086ef7a51adada50ab4bf917b7af84ac13f8309
rafik mrabet <mrabetr@hotmail.com>
2020-12-30 18:51:17 +0000

    testing mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://www.clinic-app.com" }
+  config.action_mailer.perform_deliveries = true
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
