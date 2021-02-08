05cf5e1a93f0ed60323e680d612a4e306a8d5dfe
rafik mrabet <mrabetr@hotmail.com>
2021-02-08 09:29:38 +0000

    added select2 plugin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -26,14 +26,17 @@ require("channels")
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
 import { initUpdateNavbarOnScroll } from '../components/navbar';
+import { initSelect2 } from '../plugins/init_select2';
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   if (document.querySelector(".navbar-home")) {
     initUpdateNavbarOnScroll();
+  }
+  if (document.querySelector(".select2")) {
+    initSelect2();
   }
 });
 
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,2 +1,3 @@
 class Request < ApplicationRecord
+  validates :mobile, format: { with: /\A(0|(\+|00)[1-9]\d{1,2}[\s-]?)[1-9]\d{8,}\z/, message: "bad format" }
 end
