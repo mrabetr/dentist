e8e30f89763ac6c79cdcf1ae725a8dc8d29ed44e
rafik mrabet <mrabetr@hotmail.com>
2021-03-15 18:50:33 +0000

    Social Meta Tags Setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,10 @@ class ApplicationController < ActionController::Base
     bookings_path
   end
 
+  def default_url_options
+    { host: ENV["DOMAIN"] || "localhost:3000" }
+  end
+
   private
 
   def configure_permitted_parameters
