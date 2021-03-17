80256e91e2b4df881a1b52e4ab8a9a7edbfcea46
rafik mrabet <mrabetr@hotmail.com>
2021-03-17 23:35:02 +0000

    added new appointment request email notification

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,3 +1,9 @@
 class Request < ApplicationRecord
   validates :mobile, format: { with: /\A(0|(\+|00)[1-9]\d{1,2}[\s-]?)[1-9]\d{8,}\z/, message: "bad format" }
+
+  after_commit :send_email, on: :create
+
+  def send_email
+    RequestMailer.request_notification.deliver!
+  end
 end
