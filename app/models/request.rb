c1cdff94a1ad9d944bf6b2eb47f3d6972cc68f3a
rafik mrabet <mrabetr@hotmail.com>
2022-05-19 19:35:15 +0100

    added email to customer on request

--- app/models/request.rb
+++ app/models/request.rb
@@ -2,8 +2,13 @@ class Request < ApplicationRecord
   validates :mobile, format: { with: /\A(0|(\+|00)[1-9]\d{1,2}[\s-]?)[1-9]\d{8,}\z/, message: "bad format" }
 
   after_commit :send_email, on: :create
+  after_commit :send_acknowledgement, on: :create
 
   def send_email
     NotificationMailer.request_notification.deliver!
+  end
+
+  def send_acknowledgement
+    NotificationMailer.with(request: self).request_acknowledgement.deliver!
   end
 end
