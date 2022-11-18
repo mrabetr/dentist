fee215d232ab4b17506a6730b72f9a382ef4b1d1
rafik mrabet <mrabetr@hotmail.com>
2022-11-18 17:59:35 +0000

    upgraded to rails 7

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Dentist
       generate.test_framework :test_unit, fixture: false
     end
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
