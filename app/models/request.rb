278769549a6f685d9117bdd4f168240290e60f98
rafik mrabet <mrabetr@hotmail.com>
2023-01-19 20:54:14 +0000

    removed request email and updated acknowledgement email

--- app/models/request.rb
+++ app/models/request.rb
@@ -4,7 +4,7 @@ class Request < ApplicationRecord
 
   validates :mobile, format: { with: /\A(0|(\+|00)[1-9]\d{1,2}[\s-]?)[1-9]\d{8,}\z/, message: "bad format" }
 
-  after_commit :send_email, on: :create
+  # after_commit :send_email, on: :create
   after_commit :send_acknowledgement, on: :create
 
   def send_email
