b772fdbb3c1c4831bf390a4729ef17a977f6a33c
rafik mrabet <mrabetr@hotmail.com>
2023-01-19 19:18:06 +0000

    added invisible_captcha

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,7 @@
 class Request < ApplicationRecord
+  # Virtual attribute for invisible_captcha to work
+  attr_accessor :remote_ip
+
   validates :mobile, format: { with: /\A(0|(\+|00)[1-9]\d{1,2}[\s-]?)[1-9]\d{8,}\z/, message: "bad format" }
 
   after_commit :send_email, on: :create
