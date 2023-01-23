725c82f5e48ee2f593645432bb44c22cb99b4062
rafik mrabet <mrabetr@hotmail.com>
2023-01-23 18:28:59 +0000

    add new booking redirection via JS only

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -6,57 +6,94 @@ import Rails from "@rails/ujs"
 
 export default class extends Controller {
 
+  viewCalendarBooking() {
+    let calendar = this.calendar;
+    calendar.on('clickEvent', (event) => {
+
+      Rails.ajax({
+        type: "GET",
+        url: `/bookings/${event.event.id}`,
+        success: function(result) {
+          window.location = `/bookings/${event.event.id}`;
+        },
+      })
+    })
+  }
+
   newCalendarBooking() {
     let calendar = this.calendar;
     calendar.on('selectDateTime', (event) => {
-      let formData = new FormData()
-      formData.append('[booking]start_time', event.start);
-      formData.append('[booking]end_time', event.end);
-      // console.log(formData.get('[booking]start_time'));
+      const start_time = event.start
+      const end_time = event.end
+      const date = start_time.toISOString().split('T')[0]
+      const time = start_time.toISOString().split('T')[1].substring(0, 5)
+      const length = (end_time.getTime() - start_time.getTime()) / (1000 * 60)
+
       Rails.ajax({
-        type: "POST",
-        url: '/bookings/new_booking',
-        data: formData
+        type: "GET",
+        url: `/bookings/new`,
+        success: function(result) {
+          window.location = `/bookings/new?date=${date}&time=${time}&length=${length}`;
+        },
       })
     })
-    // calendar.on('clickEvent', (event) => {
-    //   console.log('clickEvent', event)
-    // })
-    // calendar.fire('beforeCreateEvent', {
-    //   title: "test title",
-    // });
-    // calendar.on('beforeCreateEvent', (event) => {
-    //   // const booking = {
-    //   //   id: 1,
-    //   //   calendarId: 'calendar',
-    //   //   title: event.title,
-    //   //   category: 'time',
-    //   //   location: event.location,
-    //   //   start: event.start,
-    //   //   end: event.end
-    //   // }
-    //   console.log('clickEvent', event)
-    //   // event.title = 'New Booking'
-
-    //   // calendar.createEvents([booking]);
-
-    //   let formData = new FormData()
-    //   formData.append('[booking]title', event.title);
-    //   formData.append('[booking]start', event.start.toDate());
-    //   formData.append('[booking]end', event.end.toDate());
-    //   formData.append('[booking]location', event.location);
-    //   console.log(formData.get('[booking]title'));
-    //   console.log(event.goingDuration);
-    //   console.log(event.start.toDate());
-    //   console.log(event.title);
-
-    //   Rails.ajax({
-    //     type: "POST",
-    //     url: '/bookings/new_booking',
-    //     data: formData
-    //   })
-    // });
   }
+
+  // newCalendarBooking() {
+  //   let calendar = this.calendar;
+  //   calendar.on('selectDateTime', (event) => {
+  //     let formData = new FormData()
+  //     formData.append('[booking]start_time', event.start);
+  //     formData.append('[booking]end_time', event.end);
+  //     // console.log(formData.get('[booking]start_time'));
+  //     const start_time = event.start
+  //     const end_time = event.end
+  //     const length = (end_time.getTime() - start_time.getTime()) / (1000 * 60)
+  //     console.log(length)
+  //     Rails.ajax({
+  //       type: "POST",
+  //       url: '/bookings/new_booking',
+  //       data: formData
+  //     })
+  //   })
+  //   // calendar.on('clickEvent', (event) => {
+  //   //   console.log('clickEvent', event)
+  //   // })
+  //   // calendar.fire('beforeCreateEvent', {
+  //   //   title: "test title",
+  //   // });
+  //   // calendar.on('beforeCreateEvent', (event) => {
+  //   //   // const booking = {
+  //   //   //   id: 1,
+  //   //   //   calendarId: 'calendar',
+  //   //   //   title: event.title,
+  //   //   //   category: 'time',
+  //   //   //   location: event.location,
+  //   //   //   start: event.start,
+  //   //   //   end: event.end
+  //   //   // }
+  //   //   console.log('clickEvent', event)
+  //   //   // event.title = 'New Booking'
+
+  //   //   // calendar.createEvents([booking]);
+
+  //   //   let formData = new FormData()
+  //   //   formData.append('[booking]title', event.title);
+  //   //   formData.append('[booking]start', event.start.toDate());
+  //   //   formData.append('[booking]end', event.end.toDate());
+  //   //   formData.append('[booking]location', event.location);
+  //   //   console.log(formData.get('[booking]title'));
+  //   //   console.log(event.goingDuration);
+  //   //   console.log(event.start.toDate());
+  //   //   console.log(event.title);
+
+  //   //   Rails.ajax({
+  //   //     type: "POST",
+  //   //     url: '/bookings/new_booking',
+  //   //     data: formData
+  //   //   })
+  //   // });
+  // }
 
   getCalendarData() {
     let url = '/bookings.json'
@@ -145,5 +182,6 @@ export default class extends Controller {
     // console.log(firstEvent.title); // 'Weekly Meeting'
     this.getCalendarData();
     this.newCalendarBooking();
+    this.viewCalendarBooking();
   }
 }
